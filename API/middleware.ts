3655d66b38e39e5b6e46fd8a44bf9077b8efb6d6
Ali Osaid <aliusaid55@gmail.com>
2024-03-28 16:57:41 -0700

    Update API base URL

--- API/middleware.ts
+++ API/middleware.ts
@@ -5,7 +5,7 @@ const getAccessToken = () => {
 };
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, 
+  baseURL: 'http://128.199.30.51:8000/api', 
 });
 
 api.interceptors.request.use(
