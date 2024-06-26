60a060df90238d697449365d0155067c937b67e5
ali usaid <aliusaid55@gmail.com>
2024-06-26 12:05:26 -0700

    chore: Update API middleware base URL and page title

--- API/middleware.ts
+++ API/middleware.ts
@@ -5,7 +5,7 @@ const getAccessToken = () => {
 };
 
 const api = axios.create({
-  baseURL: "http://64.23.247.3:8000/api",
+  baseURL: "https://backend.improof.health/api",
 });
 api.interceptors.request.use(
   async (config) => {
